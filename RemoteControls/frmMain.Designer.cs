14733151d02dabe39a2e3367c2e05cce6c272818
Mustafa Çağrı Altındal <39138827+mcaltindal33@users.noreply.github.com>
2019-08-26 21:58:14 +0300

    Add project files.

---
+++ RemoteControls/frmMain.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace RemoteControls
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.layoutControl1 = new DevExpress.XtraLayout.LayoutControl();
+            this.Root = new DevExpress.XtraLayout.LayoutControlGroup();
+            this.tAddress = new DevExpress.XtraEditors.TextEdit();
+            this.layoutControlItem1 = new DevExpress.XtraLayout.LayoutControlItem();
+            this.tPort = new DevExpress.XtraEditors.TextEdit();
+            this.layoutControlItem2 = new DevExpress.XtraLayout.LayoutControlItem();
+            this.btnScreen = new DevExpress.XtraEditors.SimpleButton();
+            this.layoutControlItem3 = new DevExpress.XtraLayout.LayoutControlItem();
+            this.btnConnect = new DevExpress.XtraEditors.SimpleButton();
+            this.layoutControlItem4 = new DevExpress.XtraLayout.LayoutControlItem();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControl1)).BeginInit();
+            this.layoutControl1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Root)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tAddress.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tPort.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // layoutControl1
+            // 
+            this.layoutControl1.Controls.Add(this.btnConnect);
+            this.layoutControl1.Controls.Add(this.btnScreen);
+            this.layoutControl1.Controls.Add(this.tPort);
+            this.layoutControl1.Controls.Add(this.tAddress);
+            this.layoutControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.layoutControl1.Location = new System.Drawing.Point(0, 0);
+            this.layoutControl1.Name = "layoutControl1";
+            this.layoutControl1.Root = this.Root;
+            this.layoutControl1.Size = new System.Drawing.Size(290, 153);
+            this.layoutControl1.TabIndex = 0;
+            this.layoutControl1.Text = "layoutControl1";
+            // 
+            // Root
+            // 
+            this.Root.EnableIndentsWithoutBorders = DevExpress.Utils.DefaultBoolean.True;
+            this.Root.GroupBordersVisible = false;
+            this.Root.Items.AddRange(new DevExpress.XtraLayout.BaseLayoutItem[] {
+            this.layoutControlItem1,
+            this.layoutControlItem2,
+            this.layoutControlItem3,
+            this.layoutControlItem4});
+            this.Root.Name = "Root";
+            this.Root.Size = new System.Drawing.Size(290, 153);
+            this.Root.TextVisible = false;
+            // 
+            // tAddress
+            // 
+            this.tAddress.Location = new System.Drawing.Point(12, 28);
+            this.tAddress.Name = "tAddress";
+            this.tAddress.Size = new System.Drawing.Size(266, 20);
+            this.tAddress.StyleController = this.layoutControl1;
+            this.tAddress.TabIndex = 4;
+            // 
+            // layoutControlItem1
+            // 
+            this.layoutControlItem1.Control = this.tAddress;
+            this.layoutControlItem1.Location = new System.Drawing.Point(0, 0);
+            this.layoutControlItem1.Name = "layoutControlItem1";
+            this.layoutControlItem1.Size = new System.Drawing.Size(270, 40);
+            this.layoutControlItem1.Text = "Ip Address";
+            this.layoutControlItem1.TextLocation = DevExpress.Utils.Locations.Top;
+            this.layoutControlItem1.TextSize = new System.Drawing.Size(66, 13);
+            // 
+            // tPort
+            // 
+            this.tPort.Location = new System.Drawing.Point(12, 68);
+            this.tPort.Name = "tPort";
+            this.tPort.Size = new System.Drawing.Size(266, 20);
+            this.tPort.StyleController = this.layoutControl1;
+            this.tPort.TabIndex = 5;
+            // 
+            // layoutControlItem2
+            // 
+            this.layoutControlItem2.Control = this.tPort;
+            this.layoutControlItem2.Location = new System.Drawing.Point(0, 40);
+            this.layoutControlItem2.Name = "layoutControlItem2";
+            this.layoutControlItem2.Size = new System.Drawing.Size(270, 40);
+            this.layoutControlItem2.Text = "Port Numbber";
+            this.layoutControlItem2.TextLocation = DevExpress.Utils.Locations.Top;
+            this.layoutControlItem2.TextSize = new System.Drawing.Size(66, 13);
+            // 
+            // btnScreen
+            // 
+            this.btnScreen.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("btnScreen.ImageOptions.SvgImage")));
+            this.btnScreen.Location = new System.Drawing.Point(147, 92);
+            this.btnScreen.Name = "btnScreen";
+            this.btnScreen.Size = new System.Drawing.Size(131, 38);
+            this.btnScreen.StyleController = this.layoutControl1;
+            this.btnScreen.TabIndex = 6;
+            this.btnScreen.Text = "Screen";
+            this.btnScreen.Click += new System.EventHandler(this.BtnScreen_Click);
+            // 
+            // layoutControlItem3
+            // 
+            this.layoutControlItem3.Control = this.btnScreen;
+            this.layoutControlItem3.Location = new System.Drawing.Point(135, 80);
+            this.layoutControlItem3.Name = "layoutControlItem3";
+            this.layoutControlItem3.Size = new System.Drawing.Size(135, 53);
+            this.layoutControlItem3.TextSize = new System.Drawing.Size(0, 0);
+            this.layoutControlItem3.TextVisible = false;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("btnConnect.ImageOptions.SvgImage")));
+            this.btnConnect.Location = new System.Drawing.Point(12, 92);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(131, 38);
+            this.btnConnect.StyleController = this.layoutControl1;
+            this.btnConnect.TabIndex = 7;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.Click += new System.EventHandler(this.BtnConnect_Click);
+            // 
+            // layoutControlItem4
+            // 
+            this.layoutControlItem4.Control = this.btnConnect;
+            this.layoutControlItem4.Location = new System.Drawing.Point(0, 80);
+            this.layoutControlItem4.Name = "layoutControlItem4";
+            this.layoutControlItem4.Size = new System.Drawing.Size(135, 53);
+            this.layoutControlItem4.TextSize = new System.Drawing.Size(0, 0);
+            this.layoutControlItem4.TextVisible = false;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(290, 153);
+            this.Controls.Add(this.layoutControl1);
+            this.Name = "frmMain";
+            this.Text = "frmMain";
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControl1)).EndInit();
+            this.layoutControl1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.Root)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tAddress.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tPort.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem4)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraLayout.LayoutControl layoutControl1;
+        private DevExpress.XtraEditors.SimpleButton btnConnect;
+        private DevExpress.XtraEditors.SimpleButton btnScreen;
+        private DevExpress.XtraEditors.TextEdit tPort;
+        private DevExpress.XtraEditors.TextEdit tAddress;
+        private DevExpress.XtraLayout.LayoutControlGroup Root;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem1;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem2;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem3;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem4;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ RemoteControls/frmMain.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+using System.Net.Sockets;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace RemoteControls
+{
+    public partial class frmMain : DevExpress.XtraEditors.XtraForm
+    {
+        private readonly TcpClient client = new TcpClient();
+        private NetworkStream mainStream;
+        int portNumber;
+        private static Image grapDesktop()
+        {
+            Rectangle rectangle = Screen.PrimaryScreen.Bounds;
+            Bitmap screenshot = new Bitmap(rectangle.Width,
+                                           rectangle.Height,
+                                           System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+            Graphics graphics = Graphics.FromImage(screenshot);
+            graphics.CopyFromScreen(rectangle.X, rectangle.Y, 0, 0, rectangle.Size, CopyPixelOperation.SourceCopy);
+            return screenshot;
+        }
+        void SendDesktopImage()
+        {
+            BinaryFormatter binary = new BinaryFormatter();
+            mainStream = client.GetStream();
+            binary.Serialize(mainStream, grapDesktop());
+        }
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnConnect_Click(object sender, EventArgs e)
+        {
+            portNumber = int.Parse(tPort.Text);
+            try
+            {
+                client.Connect(tAddress.Text, portNumber);
+                XtraMessageBox.Show("Bağlandı");
+            }
+            catch (Exception)
+            {
+                XtraMessageBox.Show("Bağlantı hatalı");
+            }
+        }
+
+        private void BtnScreen_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
